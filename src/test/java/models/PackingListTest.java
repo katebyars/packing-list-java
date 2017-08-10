a9c8b8bc2111dd25855ffb05afb9b7013ac52652
Guest User <Guest@Epicodus-5D.local>
2017-08-10 09:22:00 -0700

    add initial commit

---
+++ src/test/java/models/PackingListTest.java
@@ -0,0 +1,21 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/10/17.
+ */
+public class PackingListTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+}
